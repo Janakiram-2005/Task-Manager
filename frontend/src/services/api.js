acc244d866aba3c4b68802fbb09dc2765690fdd4
Janakiram-2005 <msjanakiram2005@gmail.com>
2026-02-25 15:07:54 +0530

    Initial commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAuthToken } from "./authToken";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://task-manager-backend-961886344080.us-central1.run.app/api"
 });
 
 api.interceptors.request.use((config) => {
